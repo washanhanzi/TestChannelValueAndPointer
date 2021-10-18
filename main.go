f248507665263ae9357585c47b848bc2c98e9456
franc <francismajere@gmail.com>
2021-10-18 20:47:04 +0800

    nothing

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package TestChannelValueAndPointer
 
-type BigStruct struct {
+type bigStruct struct {
 	Data [500]byte
 }
 
 func main() {
-	a := BigStruct{Data: [500]byte{}}
-	c := make(chan BigStruct)
+	a := bigStruct{Data: [500]byte{}}
+	c := make(chan bigStruct)
 	go func() {
 		for i := 0; i < 100; i++ {
 			c <- a
