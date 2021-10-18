c7e9a802641b112280377d442e78d26c157aab7e
franc <francismajere@gmail.com>
2021-10-18 17:52:41 +0800

    pretty

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # TestChannelValueAndPointer
+
+- cpu: Intel(R) Core(TM) i7-8700B CPU @ 3.20GHz
+
+| type                           | iter   | ns/op       | B/op       | allocs/op   |
+| ------------------------------ | ------ | ----------- | ---------- | ----------- |
+| Benchmark/500byte_value-12     | 295989 | 4211 ns/op  | 672 B/op   | 4 allocs/op |
+| Benchmark/500byte_pointer-12   | 569635 | 3389 ns/op  | 1685 B/op  | 6 allocs/op |
+| Benchmark/1000byte_value-12    | 371932 | 4773 ns/op  | 679 B/op   | 4 allocs/op |
+| Benchmark/1000byte_pointer-12  | 688429 | 6162 ns/op  | 2718 B/op  | 6 allocs/op |
+| Benchmark/10000byte_value-12   | 100243 | 14913 ns/op | 624 B/op   | 4 allocs/op |
+| Benchmark/10000byte_pointer-12 | 174306 | 21985 ns/op | 21231 B/op | 6 allocs/op |
--- value_test.go
+++ value_test.go
@@ -4,34 +4,101 @@ import (
 	"testing"
 )
 
-type BigStruct struct {
-	Data [500]byte
-}
-
-func BenchmarkValue(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		c := make(chan BigStruct)
-		go func() {
-			for i := 0; i < 100; i++ {
-				a := BigStruct{Data: [500]byte{}}
-				c <- a
-			}
-		}()
-		d := <-c
-		d.Data[0] = 1
-	}
-}
-
-func BenchmarkPointer(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		c := make(chan *BigStruct)
-		go func() {
-			for i := 0; i < 100; i++ {
-				a := BigStruct{Data: [500]byte{}}
-				c <- &a
-			}
-		}()
-		d := <-c
-		d.Data[0] = 1
-	}
+func Benchmark(b *testing.B) {
+	b.Run("500byte value", func(b *testing.B) {
+		type bigStruct struct {
+			Data [500]byte
+		}
+		for n := 0; n < b.N; n++ {
+			c := make(chan bigStruct)
+			go func() {
+				for i := 0; i < 100; i++ {
+					a := bigStruct{Data: [500]byte{}}
+					c <- a
+				}
+			}()
+			d := <-c
+			d.Data[0] = 1
+		}
+	})
+	b.Run("500byte pointer", func(b *testing.B) {
+		type bigStruct struct {
+			Data [500]byte
+		}
+		for n := 0; n < b.N; n++ {
+			c := make(chan *bigStruct)
+			go func() {
+				for i := 0; i < 100; i++ {
+					a := bigStruct{Data: [500]byte{}}
+					c <- &a
+				}
+			}()
+			d := <-c
+			d.Data[0] = 1
+		}
+	})
+	b.Run("1000byte value", func(b *testing.B) {
+		type bigStruct struct {
+			Data [1000]byte
+		}
+		for n := 0; n < b.N; n++ {
+			c := make(chan bigStruct)
+			go func() {
+				for i := 0; i < 100; i++ {
+					a := bigStruct{Data: [1000]byte{}}
+					c <- a
+				}
+			}()
+			d := <-c
+			d.Data[0] = 1
+		}
+	})
+	b.Run("1000byte pointer", func(b *testing.B) {
+		type bigStruct struct {
+			Data [1000]byte
+		}
+		for n := 0; n < b.N; n++ {
+			c := make(chan *bigStruct)
+			go func() {
+				for i := 0; i < 100; i++ {
+					a := bigStruct{Data: [1000]byte{}}
+					c <- &a
+				}
+			}()
+			d := <-c
+			d.Data[0] = 1
+		}
+	})
+	b.Run("10000byte value", func(b *testing.B) {
+		type bigStruct struct {
+			Data [10000]byte
+		}
+		for n := 0; n < b.N; n++ {
+			c := make(chan bigStruct)
+			go func() {
+				for i := 0; i < 100; i++ {
+					a := bigStruct{Data: [10000]byte{}}
+					c <- a
+				}
+			}()
+			d := <-c
+			d.Data[0] = 1
+		}
+	})
+	b.Run("10000byte pointer", func(b *testing.B) {
+		type bigStruct struct {
+			Data [10000]byte
+		}
+		for n := 0; n < b.N; n++ {
+			c := make(chan *bigStruct)
+			go func() {
+				for i := 0; i < 100; i++ {
+					a := bigStruct{Data: [10000]byte{}}
+					c <- &a
+				}
+			}()
+			d := <-c
+			d.Data[0] = 1
+		}
+	})
 }
